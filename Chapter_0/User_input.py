fd959b556a3a910a04f8cedc50a6a04bf71f14f4
Khushi Yadav <khushiyadav1254@gmail.com>
2026-05-31 17:09:40 +0530

    Create User_input.py for user input demonstration
    Added a sample program to demonstrate user input in Python.

---
+++ Chapter_0/User_input.py
@@ -0,0 +1,12 @@
+#program to take input from the user
+print("a sample program to understand input method in python")
+
+name=input("enter your good name:")
+
+print("your good name is:",name)
+
+#assignment: write input of your age
+age=input("enter your age:")
+
+
+
