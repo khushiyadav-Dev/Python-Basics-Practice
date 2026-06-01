d835198ec8f4f295d4b3a2796f7b38d989dd14c4
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-01 23:04:04 +0530

    Add summary of data types and operators

---
+++ Sumaary_ch_2.py
@@ -0,0 +1,9 @@
+data types = define what kind of value a variable can hold.
+
+keywords= are reserved words with predefined meanings.
+
+print sum program= helps understand input, conversion,and output.
+
+type conversion = can be implicit (auto) or explicit(manual).
+
+operators= perform action like math , comparison,logic, etc .
