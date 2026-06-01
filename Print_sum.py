e8b38554e66081d5cc352d4e8bf74ee41abff9b0
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-01 11:42:23 +0530

    Implement sum calculation for two user inputs

---
+++ Print_sum.py
@@ -0,0 +1,6 @@
+#Question :input two numbers and print sum
+
+input1= int(input("enter first number"))
+input2= int(input("enter second number"))
+sum = input1+input2
+print("sum of the two value is:",sum)
