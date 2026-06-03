0ba8fee31e929bed3cff8f5dee215e9bdad195a9
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-03 14:05:01 +0530

    Implement string functions in 03_str_functions.py
    Add string manipulation functions and examples.

---
+++ 03_str_functions.py
@@ -0,0 +1,9 @@
+name = "khushi"
+
+print = (len(name))#lenght btayega name ka jaise kitne letter h to isme 6 letter h to 6 hoga
+
+print(name.endswith("shi"))#print ho jayega kyuki last me shi h ager SHI likhte to print nhi hota
+
+print(name.startwith("kh"))#output me kh true ho jayega 
+
+print(name.capitalize())# starting vale letter ko capital kr dega
