84d970403d9880f013bcf2580d90eade1015abf7
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-04 10:44:47 +0530

    Create Ch05_practice_sets.py with dictionary and set problems
    Added a Python program with various problems related to dictionaries and sets.

---
+++ Ch05_practice_sets.py
@@ -0,0 +1,50 @@
+# problem1: write a program to crate a dictionary of hindi words with their values as well as Eng translation.
+
+words = (
+    "madad": "help"
+    "kursi": "chair"
+    "billi": "cat"
+)
+
+word = input("enter the word you want meaning of : ")
+
+print(words[word])
+
+#problem2: write a program to input eight numbers from the user and display all the unique numbers (once)
+
+#problem3: can we have a set with 18(int) and "18"(str) as a value in it?
+
+#problem4: what will be the length of following sets S: 
+ s = set()
+ s = add(20)
+ s = add(20.0)
+ s = add("20") #length of s after these operations
+
+# problem5: s = 0 what is the type of 's'?
+
+#problem6: create an empty dictionary allow 4 friends to enter their favourite language as value and use key as their names.assume that the names are unique.
+
+#problem7: if the names of 2 friends are same ,what will happen to the program in problem 6?
+
+#problem8: if languages of 2 friends are same ; what will happen to the program in problem 6?
+
+#problem9: can you change the values inside a list which is contained in set 5?
+a = { 8,7,17,"khushi", (1,2)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
