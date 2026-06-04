81fba411ad7809204a9dc60290bc513677dc5489
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-04 10:21:23 +0530

    Implement set union and intersection examples
    Added examples for set union and intersection in Python.

---
+++ Set_union_intrsec.py
@@ -0,0 +1,41 @@
+s1 = {1,45,6} # 1 subset h kyuki ye element h s1 me
+s2 = {7,8,1,78}
+
+print(s1.union(s2))
+print(s1.intersection(s2))# output: 1
+
+ Set Union ( ∪ )
+Union ka matlab: dono sets ke saare unique elements ek saath
+
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+result = A.union(B)
+print(result)
+
+# output{1, 2, 3, 4, 5}
+
+
+Set Intersection ( ∩ )
+👉 Intersection ka matlab: common elements (jo dono sets me same ho)
+✅ Example:
+Python
+A = {1, 2, 3}
+B = {2, 3, 4}
+
+result = A.intersection(B)
+print(result)
+
+Output:
+
+{2, 3}
+👉 Shortcut operator:
+Python
+result = A & B
+
+
+
+
+
+
+
