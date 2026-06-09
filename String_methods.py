fb0ff8737079f37132814d3c5b58dbb9d90e8816
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-09 07:07:49 +0530

    Create String_methods.py

---
+++ String_methods.py
@@ -0,0 +1,27 @@
+name = "chutki"
+print(name.upper()) #CHUTKI
+print(name.lower()) #chutki
+
+.replace()
+
+sentence = "i am happy today"
+new_sentence = sentence.replace("happy","sad")
+print(new_sentence)
+
+#o/p i am sad today
+
+.split()
+data = "chutki,coding,python"
+words = data.split(",")
+print(words)
+
+
+my_text = "i love python coding"
+print(my_text.upper())
+new_sentence = my_text.replace("python","java")
+print(new_sentence)
+
+
+
+
+
