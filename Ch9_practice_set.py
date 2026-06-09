8e99f3b2631eebcaddb30758e3e799497c184835
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-09 09:50:53 +0530

    Create Ch9_practice_set.py with programming tasks
    Added practice set exercises for file handling and text manipulation.

---
+++ Ch9_practice_set.py
@@ -0,0 +1,20 @@
+CHAPTER 9 – PRACTICE SET
+1)Write a program to read the text from a given file poems.txt and find out whether it contains the word 'twinkle'.
+The game() function in a program lets a user play a game and returns the score as an integer. You need to read a file Hi-score.txt which is either blank or contains the previous Hi-score. You need to write a program to update the Hi-score whenever the game() function breaks the Hi-score.
+2)Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13-year-old.
+A file contains a word "Donkey" multiple times. You need to write a program which replaces this word with ##### by updating the same file.
+Repeat program 4 for a list of such words to be censored.
+3)Write a program to mine a log file and find out whether it contains 'python'.
+4)Write a program to find out the line number where python is present from ques 6.
+5)Write a program to make a copy of a text file "this.txt".
+6)Write a program to find out whether a file is identical & matches the content of another file.
+7)Write a program to wipe out the content of a file using python.
+8)Write a python program to rename a file to "renamed_by_python.txt".
+
+
+
+
+
+
+
+
