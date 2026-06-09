ba406d9cfddeefc143a6793c46eb9a37faa0669b
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-09 09:06:09 +0530

    P

---
+++ Project_1.py
@@ -0,0 +1,38 @@
+
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+computer = -1
+youstr = input("enter your choice : ")
+
+youDict = {"s": 1, "w": -1, "g": 0}
+you = youDict[yourstr]
+
+if(computer == -1 and you == 1):
+    print("You win!")
+
+elif(computer == -1 and you == 0):
+    print("You Lose!")
+
+elif(computer == 1 and you == -1):
+    print("You lose!")
+
+elif(computer == 1 and you == 0):
+    print("You win!")
+
+elif(computer == 0 and you == -1):
+    print("You win!")
+
+elif(computer == 0 and you == 1):
+    print("You Lose!")
+
+else:
+    print("Something went wrong!")
+
+
+
+
+
+
