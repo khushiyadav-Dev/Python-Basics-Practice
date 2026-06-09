0507b38aa6622946c91bfc67bcf03f3ced6e5983
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-09 09:58:41 +0530

    Add practice set exercises for Chapter 13

---
+++ Ch13_practice_sets.py
@@ -0,0 +1,5 @@
+CHAPTER 13 – PRACTICE SET
+1)Create two virtual environments, install few packages in the first one. How do you create a similar environment in the second one?
+2)Write a program to input name, marks and phone number of a student and format it using the format function like below:
+“The name of the student is Harry, his marks are 72 and phone number is 999998888”
+3)A list contains the multiplication table of 7. Write a program to convert it to vertical string of same numbers.
