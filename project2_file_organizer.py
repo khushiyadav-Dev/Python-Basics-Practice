3d703112959b88139158393da30838bbda23f11f
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-25 18:46:21 +0530

    Add project2_file_organizer.py for file organization
    Ek Desktop Cleaner jo automatically files ko unke type ke hisaab se folders mein sort kar deta hai (Images, Documents, Videos, etc.) aur date-wise organization provide karta hai.

---
+++ project2_file_organizer.py
@@ -0,0 +1,10 @@
+'''
+Ek Desktop Cleaner jo automatically files ko unke type ke hisaab se folders mein sort kar deta hai (Images, Documents, Videos, etc.) + date-wise organization.
+Bahut useful hai real life mein jab downloads folder mess ho jata hai.
+'''
+
+
+
+
+
+  
